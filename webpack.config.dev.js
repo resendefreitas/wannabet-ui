23d8f023e28fef58b047ff62f722f677a7d1ae36
resendefreitas <guilherme.freitas@vtex.com.br>
2016-04-10 01:06:20 -0300

    Add less and css loaders

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -2,30 +2,36 @@ var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
-  // or devtool: 'eval' to debug issues with compiled output:
   devtool: 'cheap-module-eval-source-map',
   entry: [
-    // necessary for hot reloading with IE:
-    'eventsource-polyfill',
-    // listen to code updates emitted by hot middleware:
+    'eventsource-polyfill', // necessary for hot reloading with IE
     'webpack-hot-middleware/client',
-    // your code:
     './src/index'
   ],
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'bundle.js',
-    publicPath: '/dist/'
+    publicPath: '/static/'
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NoErrorsPlugin()
   ],
   module: {
-    loaders: [{
-      test: /\.js$/,
-      loaders: ['babel'],
-      include: path.join(__dirname, 'src')
-    }]
+    loaders: [
+      {
+        test: /\.less$/,
+        loader: 'style-loader!css-loader!less'
+      },
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader'
+      },
+      {
+        test: /\.jsx?/,
+        loaders: ['babel'],
+        include: path.join(__dirname, 'src')
+      }
+    ]
   }
 };
